051a8d64eff802084758361e5861450d2834224e
Austin Hussain <austin.hussain@gmail.com>
2018-11-05 20:50:57 -0800

    init commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-var firstNameOfAustinHussain = 'Austin';
+var description = 'My name is Austin Hussain, and I hope to gain a thorough understanding of how Javascript works.';
 // here's a comment
 
-
-console.log(name);
+console.log(description);
 
