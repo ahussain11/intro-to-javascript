cfc0f3a097de7fdc55811584884cd942fd2efcf8
Austin Hussain <austin.hussain@gmail.com>
2018-11-07 20:11:51 -0800

    dayTwo done

---
+++ js/scripts.js
@@ -0,0 +1,76 @@
+// // String
+// var name = "Austin";
+
+// // String Object
+// var nameTwo = new String("Austin");
+// console.log(name == nameTwo);
+
+// // Number
+// var luckyNumber = 42;
+// // Boolean
+// var boolean = true;
+// // Undefined means lack of definition
+// var mystery;
+// console.log(typeof(mystery));
+// // Null means lack of value; comes out as 'object'?
+// console.log(typeof(null));
+// // Object 
+
+// // Symbol
+
+// Global Scope
+// function() {
+//     // Scoped to function
+// }
+
+// var name = "Austin";
+// console.log(name);
+
+// {
+//     const name = "Alex";
+//     console.log(name);
+// }
+
+// console.log(name);
+
+
+
+// const name = "Bob";
+
+// console.log(`Lorem ipsum dolor sit amet ${name} consectetur adipisicing elit. Nesciunt cupiditate quo nobis earum. A officiis eligendi dolorum temporibus tempora adipisci fugiat repudiandae. Reiciendis totam molestias quam voluptates, quibusdam fugit dolor.`);
+
+// const date = new Date();
+
+// console.log(date);
+
+// console.log('slkfpoaeapoeubrg[oera[ouergoandk;jgnpoeubra[oerngpiajdglabehrlgiaberhibgaeligrlieuageirbg'.length);
+
+// const num = new Number(42);
+// console.log(typeof(num));
+
+
+// const array = [1, 3];
+// const array2 = new Array(1, 3);
+// const arrayMixed = ['Austin', 21, true];
+
+// console.log(arrayMixed);
+
+// const obj = {
+//     name: 'Austin',
+//     age: 21
+// };
+
+// console.log(obj);
+
+
+
+
+// alert('Danger Will Robinson!!');
+// const response = confirm('Are you sure?');
+
+// console.log(response);
+const favNum = prompt('what is your favorite number?');
+
+const realNum = parseInt(favNum);
+
+console.log(favNum);
