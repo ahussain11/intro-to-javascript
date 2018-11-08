fba210afbd7587ff900f2475176471d31358e4e0
Austin Hussain <austin.hussain@gmail.com>
2018-11-08 09:18:48 -0800

    init commit dayThree

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// const obj1 = {
+//     name: 'Austin'
+// };
+
+// const obj2 = {
+//     name: 'Austin'
+// };
+
+// const arr1 = ['Cat'];
+// const arr2 = ['Cat'];
+
+// console.log(obj1.toString() === obj2.toString());
+
+
+const num = '42';
+console.log(num);
+console.log(+num);
