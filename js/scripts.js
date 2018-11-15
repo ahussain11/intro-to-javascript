8d90a4a57b92b4423d8d0f28a781f47c421ded76
Austin Hussain <austin.hussain@gmail.com>
2018-11-14 19:15:53 -0800

    test

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// Arrays always start at 0, NOT 1
+// Can store multiple data types
+
+// const myFirstArray = ['Austin', 21, true, 'Salinas', ['Alex', 'Kristi', 'Taj']];
+
+// console.log(myFirstArray[4][1]);
+
+// const geekwise = [['Web for Beginners', true], ['Mobile Friendly', true], ['Intro to JS', false]];
+
+// console.log(geekwise[1][0]);
+// console.log(geekwise[0][0]);
+// console.log(geekwise[2]);
+
+// const myList = ['Mad Max', 17, true, 2015];
+
+// console.log(myList,myList.length,myList[1]);
+
+// const arr = ['Cat'];
+
+// console.log(arr[10]); --> undefined
+
